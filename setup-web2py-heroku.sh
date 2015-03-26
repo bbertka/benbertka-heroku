5b1e71c492d2b5106dde196850db3437b6ff5320
root <root@emc.com>
2015-03-26 03:22:20 +0000

    Second Commit

--- setup-web2py-heroku.sh
+++ setup-web2py-heroku.sh
@@ -3,15 +3,10 @@ sudo pip install virtualenv
 virtualenv venv --distribute
 source venv/bin/activate
 sudo pip install psycopg2
-pip freeze > requirements.txt
 echo "web: python web2py.py -a '$passwd' -i 0.0.0.0 -p \$PORT" > Procfile
-
 git init
 git add .
 git add Procfile
 git commit -a -m "Initial Commit"
-heroku create mitrend
+heroku create
 git push heroku master
-heroku addons:add heroku-postgresql:dev
-heroku scale web=1
-#heroku open
